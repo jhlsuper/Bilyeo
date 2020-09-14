e77d09a2830e2a31a962f397ea507974c74f55ad
Jeff Lee <jhlsuper@gmail.com>
2020-09-14 23:14:11 +0900

    project 시작!

---
+++ reactfrontend/bilyeo-app/src/stories/InputStory.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import SaveButton from "../components/SaveButton";
+
+// storiesOf("SaveButton", module).add("기본 설정", () => <SaveButton />);
