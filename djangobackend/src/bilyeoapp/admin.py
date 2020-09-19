ff897584f3284d5fcdf17cab8001cd18f3804132
myunbongs <myunbongs@naver.com>
2020-09-19 15:44:06 +0900

    bilyeoapp내 모델 수정

--- djangobackend/src/bilyeoapp/admin.py
+++ djangobackend/src/bilyeoapp/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-from .models import USER, CHAT, NOTIFICATION, PAYMENT, FAQ, REVIEW, REQUEST
+from .models import USER
 
 admin.site.register(USER)
-admin.site.register(CHAT)
-admin.site.register(NOTIFICATION)
-admin.site.register(PAYMENT)
-admin.site.register(FAQ)
-admin.site.register(REVIEW)
-admin.site.register(REQUEST)
